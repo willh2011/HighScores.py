60ea6caac433a57818fab48965a74468a77b37dd
willh2011 <44405453+willh2011@users.noreply.github.com>
2018-11-01 09:44:36 -0500

    Update HighScores.py

--- HighScores.py
+++ HighScores.py
@@ -1,6 +1,8 @@
 '''
 Developers: William Haskins, Edward Allington, Dylan McKinney
 '''
+loop = 0
+loop2 = 999
 #players = [["Player1", "Score"], ["Player2", "Score"], ["Player3", "Score"], ["Player4", "Score"], ["Player5", "Score"]]
 players = []
 scores = []
@@ -16,6 +18,13 @@ while entries < 5:
     entries += 1
 for i in range(len(scores)):
     print([1, 2, 3, 4, 5])
+    
+while loop < 3:
+    if scores[0] > loop2:
+        print(scores[0])
+        loop += 1
+        
+    loop2 -= 1
 
 # scoresEntered = 0
 # while scoresEntered < 5:
