6764a61e9289065ce79e94126019b2ae42919801
ALLINGTON <ea0911399@otc.edu>
2018-11-01 09:25:06 -0500

    added variables, and loops

--- HighScores.py
+++ HighScores.py
@@ -1,3 +1,21 @@
 '''
 Developers: William Haskins, Edward Allington, Dylan McKinney
 '''
+#players = [["Player1", "Score"], ["Player2", "Score"], ["Player3", "Score"], ["Player4", "Score"], ["Player5", "Score"]]
+players = []
+scores = []
+entries = 0
+playerEntry = ''
+scoreEntry = ''
+top3 = ''
+while entries < 5:
+    playerEntry = input("What is the player's name?")
+    players.append(playerEntry)
+    scoreEntry = input(f"What is {playerEntry} score? ")
+    scores.append(scoreEntry)
+    entries += 1
+for i in range(len(scores)):
+    print([1, 2, 3, 4, 5])
+
+# scoresEntered = 0
+# while scoresEntered < 5:
